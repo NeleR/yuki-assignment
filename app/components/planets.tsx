0ea2207817eb3be7a1fc161b680df96d38a8c005
NeleR <smilieke611@hotmail.com>
2025-07-26 20:21:42 +0200

    feat: list all planet names and link to their detail page

--- app/components/planets.tsx
+++ app/components/planets.tsx
@@ -1,5 +1,19 @@
+import { Link } from "react-router";
+
 type PlanetsProps = { planets: any[] };
 
 export default function Planets({ planets }: PlanetsProps) {
-  return `I am the planet overview page. There are ${planets.length} planets available`;
+  return (
+    <>
+      <h1>Star Wars Planets</h1>
+      <p>There are {planets.length} planets available</p>
+      <ol>
+        {planets.map((planet, index) => (
+          <li key={index}>
+            <Link to={`/${index}`}>{planet.name}</Link>
+          </li>
+        ))}
+      </ol>
+    </>
+  );
 }
